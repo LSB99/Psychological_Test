a9e8655de34217bcd4c345b921046df529243c57
beanxx <tnqls991211@naver.com>
2021-08-20 07:45:51 +0900

    시작 페이지, 댓글 페이지 생성, 질문 페이지 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import {withRouter} from "react-router";
 
 import StatusBar from "./StatusBar";
 import QnA from "./QnA";
 import Result from "./Result";
+import Start from "./Start";
+import Comment from "./Comment";
 
- const App = (props) => {
-  return(
-    <div className="App">
-      <StatusBar></StatusBar>
-      <QnA></QnA>
-      <Result></Result>
-  </div>
-  );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={Start}/>
+                    <Route exact path="/QnA" component={QnA}/>
+                    <Route exact path="/StatusBar" component={StatusBar}/>
+                    <Route exact path="/Result" component={Result}/>
+                    <Route exact path="/Comment" component={Comment}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
-//브랜치 생성
---
+++ src/Comment.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import img from "./back.png";
+
+const Comment = (props) => {
+    const goBack = () => {
+        props.history.goBack();
+    };
+    return (
+        <div className="container">
+            <div className="back-div">
+                <BackIcon
+                    src={img} onClick={goBack}
+                />
+            </div>
+            <div className="comment-title-div">
+                <p className="comment-title">
+                    테스트는 재미있었나요?<br/>
+                    자유롭게 테스트에 대한<br/>
+                    코멘트를 달아 보세요:)
+                </p>
+            </div>
+            <div className="comment-container">
+                <CommentItem>
+                    <CommentName>익명1</CommentName>
+                    <CommentContent>재밌어요~</CommentContent>
+
+                </CommentItem>
+            </div>
+
+            <div className="input-container">
+                <CommentInput id="comment-input" placeholder="한줄평을 입력해보세요"/>
+                <OkButton>확인</OkButton>
+            </div>
+        </div>
+    );
+}
+
+const CommentInput = styled.textarea`
+  padding: 7px;
+  border: 1px solid #BDBDBD;
+  border-radius: 5px;
+  width: 70%;
+  height: 70px;
+  text-align: left;
+  font-family: '함초롬돋움';
+`
+
+const OkButton = styled.div`
+    padding: 10px 10px;
+    background-color: #6799FF;
+    border-radius: 10px;
+    border: #F8257E;
+    color: white;
+    margin-left: 15px;
+    font-size: 13px;
+`
+
+const BackIcon = styled.img`
+    width: 9%;
+    margin-left: 10px;
+    margin-top: 5px;
+`
+
+const CommentItem = styled.div`
+  width: 80vw;
+  margin: 8px auto;
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  border: 1px solid #FFF2E6;
+  padding: 10px;
+  background-color: #FFF2E6;
+  box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.2);
+  
+`
+
+const CommentName = styled.div`
+    font-size: 15px;
+    font-weight: bold;
+`
+
+const CommentContent = styled.div`
+    text-align: center;
+`
+
+export default Comment;
