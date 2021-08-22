caf2e1b1da1019cb634c599d0f9ec3f54a03495e
zeroyouth <choi46910@naver.com>
2021-08-23 04:02:23 +0900

    ADD Result

---
+++ src/Start.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Start = (props) => {
+    return (
+        <div className="container">
+            <h1>
+                캐릭터들로 보는<br/>나의 성격 유형 테스트
+            </h1>
+            <p className="start-subtitle">나와 닮은 캐릭터는 누구일까?</p>
+            <button
+                className="start-button"
+                onClick={() => {
+                    props.history.push("/QnA");
+                }}
+            >
+                START
+            </button>
+        </div>
+    );
+};
+
+export default Start;
+
--- src/StatusBar.js
+++ src/StatusBar.js
@@ -1,11 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+// import { useSelector } from "react-redux";
 
 const StatusBar = (props) => {
+// 질문 리스트 가지고 오기
+// const question = useSelector((state) => state.question.question);
+// // 유저 선택 리스트 가지고 오기
+// const choice = useSelector((state) => state.question.choice);
+// // 선택 리스트 갯수 세기
+//   let count = choice.length;
+
     return (
+        <div>
+        <HighLight/>
         <State><div>1/8</div></State>
+        </div>
     );
-}   
+};
 
 const State = styled.div`
     & > div {
@@ -22,4 +33,52 @@ const State = styled.div`
     margin-left: 40px;
     margin-top: 20px;
 `;
+
+const HighLight = styled.div`
+  background: #df402c88;
+  height: 20px;
+//   width: ${(props) => props.width};
+  width: 20%;
+  transition: width 1s;
+  border-radius: 10px;
+`;
+
 export default StatusBar;
+
+
+//   return (
+//     <ProgressBar>
+//       <HighLight width={(count / quiz_list.length) * 100 + "%"} />
+//       <Dot />
+//     </ProgressBar>
+//   );
+// };
+
+// const ProgressBar = styled.div`
+//   width: 80%;
+//   margin: 20px auto;
+//   background: #eee;
+// //   width: 100%;
+//   height: 20px;
+//   display: flex;
+//   align-items: center;
+//   border-radius: 10px;
+// `;
+
+// const HighLight = styled.div`
+//   background: #df402c88;
+//   height: 20px;
+//   width: ${(props) => props.width};
+//   transition: width 1s;
+//   border-radius: 10px;
+// `;
+
+// const Dot = styled.div`
+//   background: #fff;
+//   border: 5px solid #df402c88;
+//   box-sizing: border-box;
+//   margin: 0px 0px 0px -10px;
+//   width: 40px;
+//   height: 40px;
+//   border-radius: 20px;
+// `;
